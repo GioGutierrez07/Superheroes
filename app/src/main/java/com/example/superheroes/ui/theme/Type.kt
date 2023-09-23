2d75bf414e6d420c6226ec3d450bc39ecdd5d3ee
giova@LAPTOP-SS13RSNT <s20120005@alumnos.itsur.edu.mx>
2023-09-23 15:36:36 -0600

    se agrego AnimatedVisibility y la funcion de la lista de heroes

--- app/src/main/java/com/example/superheroes/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Type.kt
@@ -12,7 +12,6 @@ val Cabin = FontFamily(
     Font(R.font.cabin_regular, FontWeight.Normal),
     Font(R.font.cabin_bold, FontWeight.Bold)
 )
-// Set of Material typography styles to start with
 val Typography = Typography(
 
     bodyLarge = TextStyle(
